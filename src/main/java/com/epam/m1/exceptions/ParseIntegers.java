f45bb9e2d1c020ae8221e42a24ea803f1e24fda1
davlatovv <zafargithub@gmail.com>
2023-03-18 14:05:07 +0500

    Try catch

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -21,11 +21,17 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            int number = 0;
+            try {
+                number = Integer.parseInt(next);
+                sum += number;
+            } catch (NumberFormatException e) {
+                justWords += next + " ";
+                
+            }
         }
         System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+        System.out.println("Just words: " + justWords);
     }
 }
 
